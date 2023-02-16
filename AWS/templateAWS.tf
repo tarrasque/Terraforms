aa60aa7b4f6e43a6de54e4ce458fa1467884e9b3
Luca Galliani <lucagallianimail@gmail.com>
2023-02-16 20:31:50 +0100

    added disk specification
    root_block_device

--- AWS/templateAWS.tf
+++ AWS/templateAWS.tf
@@ -7,4 +7,8 @@ provider "aws" {
 resource "aws_instance" "centosProva" {
         ami = "ami-0afcbcee3dfbce929"
         instance_type ="t2.micro"
+        root_block_device {
+                volume_size = 12
+                volume_type = "gp2"
+        }
 }
